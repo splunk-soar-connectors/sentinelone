cfbd5e92c4073a2bab218c74db0a6d7b76d5a2f7
haritas-crest <haritas@splunk.com>
2022-01-31 13:50:53 +0530

    SentinelOne[PAPP-22067]: fix pipeline failure

--- __init__.py
+++ __init__.py
@@ -1,8 +1,5 @@
 # File: __init__.py
 # Copyright (c) SentinelOne, 2018-2022
-
-
-
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
